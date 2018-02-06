92a842464409c922eb24066335245ca9e217acae
Rich Reneau <rich.reneau@gmail.com>
2018-02-06 15:38:40 -0500

    ability to update and retrieve content

--- src/App.js
+++ src/App.js
@@ -9,17 +9,48 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      titleNum: null
+      titleNum: 0,
+      data: [
+        {
+          title: "Distant Earth-like planets could harbor water — and maybe life",
+          content: "Several planets in a distant solar system have temperatures that could sustain liquid water, thought to be a key for life, a series of studies released Monday report."
+        },
+        {
+          title: "Bitcoin price drops below $8,000 for first time since November 24",
+          content: "Multiple virtual currencies have dropped significantly as regulators voiced concerns about them and worries grew over suggestions that the price of bitcoin has been propped up by popular exchange Bitfinex."
+        },
+        {
+          title: "Here's how Alicia Vikander gained 12 pounds (of muscle) for 'Tomb Raider'",
+          content: "At first, the weight gain was jarring for Vikander, who’s always been lean, but Lygdback assured the actress that she’d achieve the right action-hero look."
+        }
+      ]
     };
   }
   render() {
     console.log(this.state.titleNum);
     return (
         <div className="container">
-          <TitleBox data={this.props.data} handleClick={this._setTitleNum} />
-          <TextBox data={this.props.data} titleNum={this.state.titleNum} />
+          <TitleBox data={this.state.data} handleClick={this._setTitleNum} />
+          <TextBox document={this.state.data[this.state.titleNum]} changeHandler={this._setNewText} />
         </div>
       );
+  }
+
+  _setNewText = (newText) => {
+    const newTextArray = this.state.data.map((doc, i) => {
+        if(i === this.state.titleNum) {
+          return {
+            title: doc.title,
+            content: newText
+          }
+        } else {
+          return doc;
+        }
+    });
+    
+    this.setState({
+      data: newTextArray
+    });
   }
 
   _setTitleNum = (num) => {
--- src/TextBox.js
+++ src/TextBox.js
@@ -2,20 +2,18 @@ import React from 'react';
 
 const TextBox = (props) => {
   
-  const textArray = props.data.map((d,i) => {
-        console.log(props.titleNum);
-        if(i === props.titleNum) {
-        return (
-            <div>
-              <p>{d.content}</p>
-            </div>);
-        };
-      })
 
   return (
       <div className="text-styles">
           <h2>CONTENT</h2>
-          {textArray}
+          <input
+            value={props.document.content}
+            onChange={ (event) => {
+              // console.log(event.target.value);
+              const enteredText = event.target.value;
+              props.changeHandler(enteredText);
+            }}
+          />
       </div>
   );
 };
